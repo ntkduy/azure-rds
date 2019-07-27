afc9c7df057c76556673584bd5d5e28dbc3d9166
Daniel NGUYEN <ntkduy1604@gmail.com>
2019-07-27 15:27:38 +0800

    change folder from private to public

--- arm-template/ex-simple-vm/deploy-ex-simple-vm.ps1
+++ arm-template/ex-simple-vm/deploy-ex-simple-vm.ps1
@@ -1,12 +1,13 @@
 Connect-AzAccount
 Get-AzSubscription
-$subscriptionName = ""
-Select-AzureSubscription -SubscriptionName $subscriptionName
+
+$subscriptionID = "2397c805-80dd-493a-be4a-b8998e36b9f7"
+Select-AzSubscription -Subscription $subscriptionID
 
 $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
 
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
-    -TemplateUri "https://raw.githubusercontent.com/ntkduy/azure-rds/master/arm-template/ex-simple-vm/azuredeploy.json?token=AELOBJXN4FP7VOK74AUCMR25HP5FW" `
-    -TemplateParameterUri "https://raw.githubusercontent.com/ntkduy/azure-rds/master/arm-template/ex-simple-vm/azuredeploy.parameters.json?token=AELOBJWVTWFSF5YUDRGOBP25HP5HW"
+    -TemplateUri "https://raw.githubusercontent.com/ntkduy/azure-rds/master/arm-template/ex-simple-vm/azuredeploy.json" `
+    -TemplateParameterUri "https://raw.githubusercontent.com/ntkduy/azure-rds/master/arm-template/ex-simple-vm/azuredeploy.parameters.json"
